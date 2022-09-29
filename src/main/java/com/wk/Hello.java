e2c71a28d4266b359191fd16347675057c919f66
wangkun <209631670@qq.com>
2022-09-29 11:14:40 +0800

    second commit

--- src/main/java/com/wk/Hello.java
+++ src/main/java/com/wk/Hello.java
@@ -3,5 +3,7 @@ package com.wk;
 public class Hello {
     public static void hello() {
         System.out.println("hello");
+        System.out.println("hello2");
+        System.out.println("hello3");
     }
 }
